24c45f1500f6a3527530da7b9a6b8cc93bc6cace
Patrick Gallagher <blockchainbuddha@gmail.com>
2024-05-15 17:26:14 -0700

    add vault entity

--- src/vault-721.ts
+++ src/vault-721.ts
@@ -2,70 +2,70 @@ import {
   Approval as ApprovalEvent,
   ApprovalForAll as ApprovalForAllEvent,
   CreateProxy as CreateProxyEvent,
-  Transfer as TransferEvent
-} from "../generated/vault721/vault721"
+  Transfer as TransferEvent,
+} from "../generated/vault721/vault721";
 import {
   Approval,
   ApprovalForAll,
   CreateProxy,
-  Transfer
-} from "../generated/schema"
+  Transfer,
+} from "../generated/schema";
 
 export function handleApproval(event: ApprovalEvent): void {
   let entity = new Approval(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.owner = event.params.owner
-  entity.approved = event.params.approved
-  entity.tokenId = event.params.tokenId
+  );
+  entity.owner = event.params.owner;
+  entity.approved = event.params.approved;
+  entity.vault = event.params.tokenId.toString();
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleApprovalForAll(event: ApprovalForAllEvent): void {
   let entity = new ApprovalForAll(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.owner = event.params.owner
-  entity.operator = event.params.operator
-  entity.approved = event.params.approved
+  );
+  entity.owner = event.params.owner;
+  entity.operator = event.params.operator;
+  entity.approved = event.params.approved;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleCreateProxy(event: CreateProxyEvent): void {
   let entity = new CreateProxy(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity._user = event.params._user
-  entity._proxy = event.params._proxy
+  );
+  entity.user = event.params._user;
+  entity.proxy = event.params._proxy;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleTransfer(event: TransferEvent): void {
   let entity = new Transfer(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.from = event.params.from
-  entity.to = event.params.to
-  entity.tokenId = event.params.tokenId
+  );
+  entity.from = event.params.from;
+  entity.to = event.params.to;
+  entity.vault = event.params.tokenId.toString(); // Convert BigInt to string
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
